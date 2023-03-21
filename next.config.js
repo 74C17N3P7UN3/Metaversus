978e97263ca9c4b81385c012ae5442b2333f111e
74C17N3P7UN3 <leocalzavara05@gmail.com>
2023-03-21 20:03:33 +0100

    Removed html lang
    (Because it fails to build on Netlify)

--- next.config.js
+++ next.config.js
@@ -3,9 +3,8 @@ const nextConfig = {
    experimental: {
       appDir: false,
    },
-   i18n: {
-      locales: ["en-US"],
-      defaultLocale: "en-US",
+   images: {
+      unoptimized: true,
    },
 };
 
